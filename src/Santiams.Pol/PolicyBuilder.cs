bc0f8349a8a57f128c3c567e026f70de0f01e3e1
Christopher Williams <cw@santiams.com>
2021-10-22 06:43:03 +0100

    More unit tests

--- src/Santiams.Pol/PolicyBuilder.cs
+++ src/Santiams.Pol/PolicyBuilder.cs
@@ -5,6 +5,7 @@ using MediatR;
 using Pol.Notifications;
 using Polly;
 using Polly.Extensions.Http;
+using Polly.Retry;
 using Polly.Timeout;
 
 namespace Pol;
@@ -12,10 +13,10 @@ namespace Pol;
 public static class PolicyBuilder
 {
     /// <summary>
-    /// Builds an <see cref="AsyncTimeoutPolicy{TResult}"/> which publishes a <see cref="TimeoutNotification"/> to <see cref="IMediator"/> on timeout.
+    /// Configures an <see cref="AsyncTimeoutPolicy{TResult}"/> policy which publishes a <see cref="TimeoutNotification"/> to <see cref="IMediator"/> on timeout.
     /// </summary>
     /// <param name="timeout">The amount of time to wait before timing out</param>
-    /// <returns>The <see cref="AsyncTimeoutPolicy{TResult}"/> instance</returns>
+    /// <returns>The configured <see cref="AsyncTimeoutPolicy{TResult}"/> instance</returns>
     public static IAsyncPolicy<HttpResponseMessage> TimeoutPolicy(TimeSpan timeout)
     {
         Task OnTimeout(Context context, TimeSpan timespan, Task? cancelledTask, Exception? exception)
@@ -32,6 +33,11 @@ public static class PolicyBuilder
         return Policy.TimeoutAsync<HttpResponseMessage>(timeout, OnTimeout);
     }
 
+    /// <summary>
+    /// Configures an <see cref="AsyncRetryPolicy{TResult}"/> which publishes a <see cref="RetryNotification"/> to <see cref="IMediator"/> on each retry.
+    /// </summary>
+    /// <param name="retryCount">The number of times to retry</param>
+    /// <returns>The configured <see cref="AsyncRetryPolicy{TResult}"/> instance</returns>
     public static IAsyncPolicy<HttpResponseMessage> RetryPolicy(int retryCount)
     {
         void OnRetry(DelegateResult<HttpResponseMessage> result, int retryAttempt, Context context)
@@ -50,6 +56,12 @@ public static class PolicyBuilder
             .RetryAsync(retryCount, OnRetry);
     }
 
+    /// <summary>
+    /// Configures an <see cref="AsyncRetryPolicy{TResult}"/> which waits between retries, publishing a <see cref="RetryNotification"/> to <see cref="IMediator"/> on each retry.
+    /// </summary>
+    /// <param name="retryCount">The number of times to retry</param>
+    /// <param name="sleepDurationProvider">A function that is called to determine the duration to wait before retrying for each failure</param>
+    /// <returns>The configured <see cref="AsyncRetryPolicy{TResult}"/> instance</returns>
     public static IAsyncPolicy<HttpResponseMessage> WaitAndRetryPolicy(int retryCount, Func<int, TimeSpan> sleepDurationProvider)
     {
         void OnRetry(DelegateResult<HttpResponseMessage> result, TimeSpan sleepDuration, int retryAttempt, Context context)
@@ -66,6 +78,10 @@ public static class PolicyBuilder
             .WaitAndRetryAsync(retryCount, sleepDurationProvider, OnRetry);
     }
 
+    /// <summary>
+    /// Configures an <see cref="AsyncRetryPolicy{TResult}"/> which retries forever (until success) publishing a <see cref="RetryNotification"/> to <see cref="IMediator"/> on each retry.
+    /// </summary>
+    /// <returns>The configured <see cref="AsyncRetryPolicy{TResult}"/> instance</returns>
     public static IAsyncPolicy<HttpResponseMessage> RetryForeverPolicy()
     {
         void OnRetry(DelegateResult<HttpResponseMessage> result, int retryAttempt, Context context)
@@ -82,7 +98,8 @@ public static class PolicyBuilder
         return StandardErrorHandlingPolicyBuilder()
             .RetryForeverAsync(OnRetry);
     }
-
+    
+    
     public static IAsyncPolicy<HttpResponseMessage> CircuitBreakerPolicy(int handledEventsAllowedBeforeBreaking, TimeSpan durationOfBreak)
     {
         void OnBreak(DelegateResult<HttpResponseMessage> result, TimeSpan durationOfBreak, Context context)
--- test/Santiams.Pol.Tests/PolicyBuilderTests.cs
+++ test/Santiams.Pol.Tests/PolicyBuilderTests.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Net;
 using System.Net.Http;
 using System.Threading;
 using System.Threading.Tasks;
@@ -7,6 +8,7 @@ using MediatR;
 using Moq;
 using Pol.Notifications;
 using Polly;
+using Polly.Retry;
 using Polly.Timeout;
 using Xunit;
 
@@ -67,9 +69,151 @@ namespace Pol.Tests
         }
 
         // RetryPolicy
-        public async Task RetryPolicy_Success_CreatesAnAsyncRetryPolicy()
+        [Fact]
+        public void RetryPolicy_Success_CreatesAnAsyncRetryPolicy()
         {
-            new TimeoutRejectedException();
+            var sut = PolicyBuilder.RetryPolicy(1);
+            sut.Should().NotBeNull();
+            sut.Should().BeOfType<AsyncRetryPolicy<HttpResponseMessage>>();
+        }
+
+        [Fact]
+        public async Task RetryPolicy_Success_CallsMediatorWithCorrectRetryNotificationOnRetry()
+        {
+            var mediator = Mock.Of<IMediator>();
+            var context = new Context
+            {
+                ["MediatR"] = mediator
+            };
+            
+            var policy = PolicyBuilder.RetryPolicy(1);
+            
+            await Assert.ThrowsAsync<TimeoutRejectedException>(()=>policy.ExecuteAsync(
+                (_, _) => throw new TimeoutRejectedException("oops"), context, CancellationToken.None));
+
+            Func<RetryNotification, bool> isMatch = tn => tn.Context == context &&
+                                                            tn.RetryAttempt == 1 &&
+                                                            tn.OfTotalRetries == 1 &&
+                                                            tn.SleepDuration == null &&
+                                                            tn.Result.Exception.GetType() == typeof(TimeoutRejectedException);
+
+            Mock.Get(mediator).Verify(m=>m.Publish(
+                It.Is<RetryNotification>(tn => isMatch(tn)), It.IsAny<CancellationToken>()));
+        }
+
+        [Fact]
+        public async Task RetryPolicy_Success_DoesNotFailIfMediatorNotConfiguredInContext()
+        {
+            var context = Mock.Of<Context>();
+            
+            var policy = PolicyBuilder.RetryPolicy(1);
+            
+            await Assert.ThrowsAsync<TimeoutRejectedException>(() =>
+                policy.ExecuteAsync((_, _) => throw new TimeoutRejectedException("oops"), context, CancellationToken.None));
+        }
+
+        // WaitAndRetryPolicy
+        [Fact]
+        public void WaitAndRetryPolicy_Success_CreatesAnAsyncRetryPolicy()
+        {
+            var sut = PolicyBuilder.WaitAndRetryPolicy(1, _ =>TimeSpan.FromMilliseconds(1));
+            sut.Should().NotBeNull();
+            sut.Should().BeOfType<AsyncRetryPolicy<HttpResponseMessage>>();
+        }
+
+        [Fact]
+        public async Task WaitAndRetryPolicy_Success_CallsMediatorWithCorrectRetryNotificationOnRetry()
+        {
+            var mediator = Mock.Of<IMediator>();
+            var context = new Context
+            {
+                ["MediatR"] = mediator
+            };
+            
+            var policy = PolicyBuilder.WaitAndRetryPolicy(1, _ =>TimeSpan.FromMilliseconds(1));
+            
+            await Assert.ThrowsAsync<TimeoutRejectedException>(()=>policy.ExecuteAsync(
+                (_, _) => throw new TimeoutRejectedException("oops"), context, CancellationToken.None));
+
+            Func<RetryNotification, bool> isMatch = tn => tn.Context == context &&
+                                                          tn.RetryAttempt == 1 &&
+                                                          tn.OfTotalRetries == 1 &&
+                                                          tn.SleepDuration == TimeSpan.FromMilliseconds(1) &&
+                                                          tn.Result.Exception.GetType() == typeof(TimeoutRejectedException);
+
+            Mock.Get(mediator).Verify(m=>m.Publish(
+                It.Is<RetryNotification>(tn => isMatch(tn)), It.IsAny<CancellationToken>()));
+        }
+
+        [Fact]
+        public async Task WaitAndRetryPolicy_Success_DoesNotFailIfMediatorNotConfiguredInContext()
+        {
+            var context = Mock.Of<Context>();
+            
+            var policy = PolicyBuilder.WaitAndRetryPolicy(1, _ => TimeSpan.FromMilliseconds(1));
+            
+            await Assert.ThrowsAsync<TimeoutRejectedException>(() =>
+                policy.ExecuteAsync((_, _) => throw new TimeoutRejectedException("oops"), context, CancellationToken.None));
+        }
+        
+        // RetryForeverPolicy
+        [Fact]
+        public void RetryForeverPolicy_Success_CreatesAnAsyncRetryPolicy()
+        {
+            var sut = PolicyBuilder.RetryForeverPolicy();
+            sut.Should().NotBeNull();
+            sut.Should().BeOfType<AsyncRetryPolicy<HttpResponseMessage>>();
+        }
+
+        [Fact]
+        public async Task RetryForeverPolicy_Success_CallsMediatorWithCorrectRetryNotificationOnRetry()
+        {
+            var mediator = Mock.Of<IMediator>();
+            var context = new Context
+            {
+                ["MediatR"] = mediator
+            };
+            
+            var policy = PolicyBuilder.RetryForeverPolicy();
+            
+            var iteration = -1;
+
+            await policy.ExecuteAsync(
+                (_, _) =>
+                {
+                    iteration++;
+                    if(iteration == 0)
+                        throw new TimeoutRejectedException("oops");
+                    return Task.FromResult(new HttpResponseMessage(HttpStatusCode.OK));
+                }, context, CancellationToken.None);
+
+            Func<RetryNotification, bool> isMatch = tn => tn.Context == context &&
+                                                          tn.RetryAttempt == 1 &&
+                                                          tn.OfTotalRetries == null &&
+                                                          tn.SleepDuration == null &&
+                                                          tn.Result.Exception.GetType() == typeof(TimeoutRejectedException);
+
+            Mock.Get(mediator).Verify(m=>m.Publish(
+                It.Is<RetryNotification>(tn => isMatch(tn)), It.IsAny<CancellationToken>()));
+        }
+
+        [Fact]
+        public async Task RetryForeverPolicy_Success_DoesNotFailIfMediatorNotConfiguredInContext()
+        {
+            var context = Mock.Of<Context>();
+            
+            var policy = PolicyBuilder.RetryForeverPolicy();
+            
+            var iteration = -1;
+
+            await policy.ExecuteAsync(
+                (_, _) =>
+                {
+                    iteration++;
+                    if(iteration == 0)
+                        throw new TimeoutRejectedException("oops");
+                    return Task.FromResult(new HttpResponseMessage(HttpStatusCode.OK));
+                }, context, CancellationToken.None);
         }
     }
 }
